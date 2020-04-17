fb2cb4d19de44a7bcedefffcb4e35e386fd48c00
Saviour <theemailreader300@gmail.com>
2020-04-17 20:35:07 +0300

    reset everything to write good code

--- data/datatypes/natives/unary/UnaryNativeFunction.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by alexey on 27/10/2019.
-//
-
-#include "UnaryNativeFunction.h"
-
-UnaryNativeFunction::UnaryNativeFunction() : PyObject("unary_native") {
-
-}
--- main.cpp
+++ main.cpp
@@ -1,33 +1,4 @@
-#include "code/reqeusters/CinRequester.h"
-#include "code/parsers/CinParser.h"
-#include "data/datatypes/primitive/PyFunction.h"
-#include "data/datatypes/primitive/PyBoolean.h"
 
 int main() {
-    CinRequester req;
-    CinParser parser;
 
-    Memory& mem = Memory::getInstance();
-    std::vector<std::string> code;
-    std::vector<std::string> vars;
-    vars.emplace_back("n");
-//    code.emplace_back("print n");
-    code.emplace_back("if n == 0");
-    code.emplace_back("\treturn 1");
-    code.emplace_back("");
-    code.emplace_back("if n == 1");
-    code.emplace_back("\treturn 1");
-    code.emplace_back("");
-    code.emplace_back("return fib(n-1) + fib(n-2)");
-
-    PyFunction f = PyFunction(code, vars);
-    int p = mem.alloc(&f);
-    std::string fN{"fib"};
-    mem.allocPointer(fN, p);
-
-    while (true){
-        parser.parseLine(req);
-    }
-
-    return 0;
 }
